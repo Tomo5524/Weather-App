81ed02804894765e116b8f96f267aabc63426eae
Tomo5524 <ttoommoosrst@gmail.com>
2020-10-24 15:55:23 +0900

    fixed bugs

--- src/components/weather.js
+++ src/components/weather.js
@@ -14,7 +14,7 @@ function Weather(props) {
   async function getIcon(iconName) {
     if (iconName) {
       const iconApi = await fetch(
-        "http://openweathermap.org/img/w/" + iconName + ".png",
+        `http://openweathermap.org/img/w/${iconName}.png`,
         {
           mode: "cors",
         }
